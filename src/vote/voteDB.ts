42645b8902ce97bf1f63ec0a4436d25e2c7d4715
abobkov <abobkov@synergy.ru>
2019-11-27 11:12:24 +0300

    20191127-1

--- src/vote/voteDB.ts
+++ src/vote/voteDB.ts
@@ -3,6 +3,10 @@ import { IVoteDB } from '../interface/IVoteDB';
 import { IVoteDtoResponse, VoteDto } from '../DTO/IVoteDto';
 import { IResultDto, IResultDtoResponse } from '../DTO/result.dto';
 
+interface ITmpVoteDB {
+  [key: string]: IResultDtoResponse;
+}
+
 @Injectable()
 export class VoteDB {
   private dataDB: IVoteDB[];
@@ -14,9 +18,8 @@ export class VoteDB {
     return { response: true };
   }
   getResult(token: IResultDto): IResultDtoResponse[] {
-    // console.log(this.dataDB);
-    const d1: any = this.dataDB.filter(x => x.token === token.token);
-    const d2: any = d1.reduce((acc, val) => {
+    const d1: IVoteDB[] = this.dataDB.filter(x => x.token === token.token);
+    const d2: ITmpVoteDB = d1.reduce((acc, val) => {
       if (acc.hasOwnProperty(val.voteFor)) {
         acc[val.voteFor].votes++;
       } else {
@@ -28,38 +31,13 @@ export class VoteDB {
       }
       return acc;
     }, {});
-    const d3: any = Object.values(d2);
-    const d4: any = d3.sort((a, b) => {
-      return b.votes - a.votes;
-    });
     let i = 1;
-    const d5 = d4.map(x => {
+    return Object.values(d2).sort((a, b) => {
+      return b.votes - a.votes;
+    }).map(x => {
       x.position = i;
       i++;
       return x;
     });
-    return d5;
-    // const data: any = this.dataDB
-    //   .filter(x => x.token === token.token)
-    //   .reduce((acc, val) => {
-    //     if (acc.hasOwnProperty(val.voteFor)) {
-    //       acc[val.voteFor].votes++;
-    //     } else {
-    //       acc[val.voteFor] = {
-    //         votes: 1,
-    //         name: val.voteFor,
-    //         position: 0,
-    //       };
-    //     }
-    //     return acc;
-    //   }, {});
-    //
-    // return Object.values(data)
-    //   .sort((a, b) => a.votes - b.votes)
-    //   .map(x => {
-    //     x.position = i;
-    //     i++;
-    //     return x;
-    //   });
   }
 }
